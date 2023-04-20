a8020c03adbd4d3d236eacb6ffab29e1b252a843
Reyan Dela Cruz <reyandelacruz@Reyans-MacBook.local>
2023-04-20 09:32:06 +0800

    add schemas

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -88,6 +88,17 @@ class RecipeViewSet(viewsets.ModelViewSet):
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters= [
+            OpenApiParameter(
+                'assigned_only',
+                OpenApiTypes.INT, enum=[0, 1],
+                description='Filter by items assigned to recipes.',
+            )
+        ]
+    )
+)
 class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,
                             mixins.UpdateModelMixin,
                             mixins.ListModelMixin,
@@ -98,7 +109,14 @@ class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,
 
     def get_queryset(self):
         """filter queryset to authenticated user."""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assinged_only = bool(
+            int(self.request.query_params.get('assinged_only', 0))
+        )
+        query_set = self.get_queryset
+        if assinged_only:
+            query_set = query_set.filter(recipe__isnull=False)
+
+        return self.queryset.filter(user=self.request.user).order_by('-name').distinct()
 
 class TagViewSet(BaseRecipeAttrViewSet):
     """Mange tags in the database."""
